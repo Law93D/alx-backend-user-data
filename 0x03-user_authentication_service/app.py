bc2884610025e26710809717a62c364a82725e01
lawden <lawtaden@gmail.com>
2024-08-12 21:10:59 +0200

    basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, request, jsonify
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Root route that returns a welcome message"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """POST /users route to register a new user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
